c2dc35a24b070efcc15caf46efc498fd21bc495a
Alex Bramhill <alexbramhill@gmail.com>
2022-11-12 15:16:54 +0000

    made dummy data more relivant for testing

--- src/dummyData/dimmyData.ts
+++ src/dummyData/dimmyData.ts
@@ -1,14 +1,25 @@
 import { DataPoint } from "../class/DataPoint";
+import { addDays } from "../helper/dateHelper";
 import { processRawDataPoint } from "../helper/processDataResponseHelper";
 
 export const getDummyData = () => {
+  const dateNow = new Date();
+  const dateStart = addDays(dateNow, -10);
+  const dateMid = addDays(dateNow, -5);
   const array = Array.from({ length: 150 }, () =>
     processRawDataPoint([
-      Math.floor(Math.random() * 100000),
-      Math.floor(Math.random() * 1000).toString(),
+      randomMinMax(dateStart.valueOf() / 1000, dateNow.valueOf() / 1000),
+      randomMinMax(-10, 20).toString(),
     ])
   );
 
-  array.push(processRawDataPoint([1000, null]));
+  array.push(processRawDataPoint([dateStart.valueOf() / 1000, null]));
+  array.push(processRawDataPoint([dateNow.valueOf() / 1000, null]));
+  array.push(processRawDataPoint([dateMid.valueOf() / 1000, null]));
+
   return array;
+};
+
+const randomMinMax = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
